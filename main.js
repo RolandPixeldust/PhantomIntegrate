5f4ef4e524a5d575f7053fd079ef6e792fbd77df
Roland Womack <67436086+RolandPixeldust@users.noreply.github.com>
2022-02-28 15:03:00 -0500

    get nft data

--- main.js
+++ main.js
@@ -15,3 +15,31 @@ function disconnectAccount()
   window.solana.disconnect();
   window.solana.on('disconnect', () => console.log("disconnected!"))
 }
+
+
+function getNFTs()
+{
+//Function to get all NFT information.
+//get NFT
+  const getAllNftData = async () => {
+    try {
+      if (connectData === true) {
+        const connect =    createConnectionConfig(clusterApiUrl("devnet"));
+        const provider = getProvider();
+        let ownerToken = provider.publicKey;
+        const result = isValidSolanaAddress(ownerToken);
+        console.log("result", result);
+const nfts = await getParsedNftAccountsByOwner({
+          publicAddress: ownerToken,
+          connection: connect,
+          serialization: true,
+        });
+        return nfts;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+}
